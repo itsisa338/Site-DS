d83ee3f262afe409120a98cf81c12474a687d6bc
itsisa338 <isabela.eduarda.kalschne@gmail.com>
2025-10-31 10:45:07 -0300

    Add menu toggle function in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function menu () {
+    let menu = document.getElementById("menu");
+    if (menu.style.display === "none" ){
+
+        menu.style.display =  "block"
+    
+    } else {
+        menu.display.display = "none"
+    }
+
+}
